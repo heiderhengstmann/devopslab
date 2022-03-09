bbeee19192d29f3b23495d52cf4ddbaa69d16ac9
heiderhengstmann <58447126+heiderhengstmann@users.noreply.github.com>
2022-03-08 21:44:15 -0300

    fixed test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Heider Hengstmann"
 
 if __name__ == '__main__':
     app.run()
